57606a754ea2f32618f8824777e3ffb309ef6198
Yue Zheng Ting <yuezhengting@gmail.com>
2024-06-30 16:48:29 +0800

    feat(vectorise): add vectorise rest controller

--- backend/services/vectorise/app/main.py
+++ backend/services/vectorise/app/main.py
@@ -3,12 +3,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.middlewares.logging import LoggingMiddleware
 from app.middlewares.cors import CorsMiddleware
+from app.api.v1.vectorise import VectoriseRestController
 
 app = FastAPI()
 
+# Maybe TODO: Move registration of middlewares to separate module
 LoggingMiddleware(app)
 CorsMiddleware(app)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Maybe TODO: Move registration of routers to separate module
+app.include_router(VectoriseRestController().get_router())
