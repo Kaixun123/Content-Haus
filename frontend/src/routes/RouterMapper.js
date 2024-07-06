8fae65d761f7880370c1b34a0c43270a2f52bc6c
jerlpjgithub <113880753+jerlpjgithub@users.noreply.github.com>
2024-07-06 21:56:34 +0800

    feat(frontend): implement basic routing

--- frontend/src/routes/RouterMapper.js
+++ frontend/src/routes/RouterMapper.js
@@ -1,9 +1,24 @@
-// Import Pages
+import { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 
+// Import Pages
+import HomePage from "../pages/HomePage";
+
+
+// Redirect to home
+const RedirectToHome = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate('/home');
+    }, [navigate]);
+
+    return null;
+}
 
 // Route Mapping
-export const PUBLIC_ROUTES = [
+export const PUBLIC_ROUTE_MAPS = [
     { path: "/", element: <RedirectToHome />},
     { path: "/home", element: <HomePage />},
+];
 
-]
