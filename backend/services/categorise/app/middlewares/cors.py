a991c424ad27cfcc5918aa3b4aa15633738add1b
Lyney <30753183+hanlim83@users.noreply.github.com>
2024-07-08 02:38:44 +0800

    Push build fixes

--- backend/services/categorise/app/middlewares/cors.py
+++ backend/services/categorise/app/middlewares/cors.py
@@ -1,21 +1,22 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from middlewares.base import Middleware
+from app.middlewares.base import Middleware
+
 
 class CorsMiddleware(Middleware):
     """
     A singleton class to apply CORS middleware to a FastAPI application.
-    
+
     This class ensures that only one instance of itself can be created and
     applies CORS settings to the FastAPI application to allow requests from
     specified origins.
-    
+
     Attributes:
         __instance (CorsMiddleware): The singleton instance of the CorsMiddleware, private.
         __app (FastAPI): The FastAPI application instance, private.
         __origins (list): A list of origins allowed to make requests to the FastAPI application, private.
-    
+
     Methods:
         __new__(cls, app: FastAPI): Creates a new instance of CorsMiddleware if one doesn't exist.
         __apply(): Applies CORS middleware settings to the FastAPI application.
@@ -35,10 +36,10 @@ class CorsMiddleware(Middleware):
         already exists, it returns that instance; otherwise, it creates a new one,
         applies the CORS settings to the provided FastAPI application, and returns
         the new instance.
-        
+
         Parameters:
             app (FastAPI): The FastAPI application to which the CORS settings will be applied.
-        
+
         Returns:
             CorsMiddleware: The singleton instance of the CorsMiddleware.
         """
